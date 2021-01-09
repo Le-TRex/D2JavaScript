d44389a374f94c0703915195ed0225ee8b962cb0
Le-TRex <clarembo@gmail.com>
2021-01-09 16:05:43 +0100

    Resolve exercises until fiscal code

--- exercices.js
+++ exercices.js
@@ -1,3 +1,7 @@
+//TODO refacto les "return true" pour mettre des ternaires ou des "return condition(s)"
+
+//PARTIE I
+//----------------------------------------------------------------------
 //Revert
 
 const reverse = (string) => {
@@ -42,7 +46,7 @@ const pascalCase = (sentence) => {
 const palindrome = (string) => {
     var stringToCheck = string;
     var reversedString =  reverse(string);
-    if (stringToCheck == reversedString) {
+    if (stringToCheck === reversedString) {
         return true;
     }else{
         return false;
@@ -65,18 +69,13 @@ const findLongestWord = (sentence) => {
 const Mapi = function([[key1, value1], [key2, value2]]) {
     this.array = [[key1, value1], [key2, value2]];
     this.mapiObject = Object.fromEntries(this.array);
-    //this.size = Object.keys(this.mapiObject).length;
-    this.size=2;
+    this.size = Object.keys(this.mapiObject).length;
 
     this.set = function (key, value) {
-
-        if (this.has(key)) {
-            this.mapiObject[key]=value;
-
-        }else{
-            this.mapiObject[key]=value;
+        if (!this.has(key)) {
             this.size +=1;
         }
+        this.mapiObject[key]=value;
     }
 
     this.delete = function (key) {
@@ -102,7 +101,7 @@ const Mapi = function([[key1, value1], [key2, value2]]) {
 }
 
 //----------------------------------------------------------------------
-//Prop access
+//Prop access -- bonus
 
 //----------------------------------------------------------------------
 //Type check
@@ -117,4 +116,101 @@ const type_check_v1 = (value, type) => {
 
 //----------------------------------------------------------------------
 //Type check V2
+
+const type_check_v2 = (value, type, array) => {
+    if(type_check_v1(value, type) && array.includes(value)) {
+        return true;
+    }else{
+        return false;
+    }
+}
+
+//----------------------------------------------------------------------
+//PARTIE II
+//----------------------------------------------------------------------
+//Hash tag
+
+const sentence = 'How the Avocado Became the Fruit of the Global Trade';
+
+const getHashTags = (sentence) => {
+    var words = sentence.split(' ').sort(function (a, b) {
+        return b.length - a.length;
+    });
+
+    var threeLongestWords = [words[0], words[1], words[2]];
+
+    return hashtags = threeLongestWords.map(word => `#${word}`);
+}
+console.log('Test Hashtags : ');
+console.log(getHashTags('How the Avocado Became the Fruit of the Global Trade'));
+console.log(getHashTags('The Helicopter Is Yellow and Greenish'));
+
+//----------------------------------------------------------------------
+//RemoveDuplicates
+
+const array1 = [0, 2, 4, 6, 8, 8];
+
+const removeDuplicates = (array) => {
+    return newArray = [...new Set(array)];
+}
+console.log('Test Remove Duplicates : ')
+console.log(removeDuplicates(array1));
+
+//----------------------------------------------------------------------
+//Intersection
+
+const array2 = [1, 2, 3, 4, 5, 6];
+
+const intersection = (array1, array2) => {
+    return filteredArray = array1.filter(value => array2.includes(value));
+}
+
+console.log('Test intersection : ')
+console.log(intersection(array1, array2));
+
+//----------------------------------------------------------------------
+//Arraydiff
+
+const arrayDiff = (array1, array2) => {
+    const firstFiltrationArray = array1.filter(value => !array2.includes(value));
+    const secondFiltrationArray = array2.filter(value => !array1.includes(value));
+    const filteredArray = [...firstFiltrationArray, ...secondFiltrationArray];
+    return removeDuplicates(filteredArray);
+}
+console.log('Test ArrayDiff : attendu [0, 1, 3, 5, 8]')
+console.log(arrayDiff(array1, array2));
+
+//----------------------------------------------------------------------
+//Combination
+
+const array = [2, 12, 2];
+
+function multiplicates(array) {
+    let multiplicator = 1;
+    let result = 1;
+
+    for (let i = 0; i < array.length ; i++) {
+        let operande = array[i];
+        result = operande * multiplicator;
+        multiplicator = result;
+    }
+    return result;
+}
+
+console.log('Test combination : ');
+console.log(multiplicates(array));
+
+//----------------------------------------------------------------------
+//Fiscal Code
+
+
+
+//----------------------------------------------------------------------
+//Least Common Multiple
+
+//----------------------------------------------------------------------
+//Merge -- bonus
+
+//----------------------------------------------------------------------
+//Comments filter -- bonus
 
